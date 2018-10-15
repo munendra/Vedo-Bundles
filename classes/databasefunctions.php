924877f49603bb9df3963154e4988b0a5fd95cec
Munendra Kumar <kumarmunendra@hotmail.com>
2018-10-15 22:57:16 +0530

    plugin activated and creat table

---
+++ classes/databasefunctions.php
@@ -0,0 +1,29 @@
+<?php class databasefunctions {
+   public function createTable() {
+       global $table_prefix, $wpdb;
+
+$tblname = 'vedoBundles';
+$wp_track_table = $table_prefix . "$tblname ";
+
+#Check to see if the table exists already, if not, then create it
+
+if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
+{
+
+    $charset_collate = $wpdb->get_charset_collate();
+    $sql = "CREATE TABLE $wp_track_table (
+id mediumint(9) NOT NULL AUTO_INCREMENT,
+CreatedAt datetime  DEFAULT now(),
+Title TEXT DEFAULT '' NOT NULL,
+Url TEXT DEFAULT '' NOT NULL,
+Description TEXT DEFAULT '' NOT NULL,
+Category Text DEFAULT '',
+DealerName Text DEFAULT '',
+PRIMARY KEY  (id)
+) $charset_collate;";
+      file_put_contents(dirname(__file__).'/log.txt', $sql);
+      require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+    dbDelta($sql);
+}
+ }
+} ?>
--- index.php
+++ index.php
@@ -8,8 +8,34 @@
    Author URI: https://github.com/munendra/
    License: GPL2
    */
+  if(!defined('vedo_bundles'))
+	define('vedo_bundles', plugin_dir_url( __FILE__ ));
+if(!defined('vedo_bundles'))
+  define('vedo_bundles', plugin_dir_path( __FILE__ ));
+  include_once dirname( __FILE__ ) . '/classes/databasefunctions.php';
+  class vedoBundles 
+  {
+    var $pluginDb;
+    public function __construct()
+    {
+     
+      register_activation_hook( __FILE__, array('vedoBundles','activePlugin'));
+ // Admin page calls:
+ add_action( 'admin_menu', array( $this, 'addAdminMenu' ) );
+ //add_action( 'wp_ajax_store_admin_data', array( $this, 'storeAdminData' ) );
+ //add_action( 'admin_enqueue_scripts', array( $this, 'addAdminScripts' ) );
+    }
 
-  include_once dirname( __FILE__ ) . '/install.php';
-  register_activation_hook( __FILE__, array( 'MyPlugin', 'install' ) );
 
-?>
+    function addAdminMenu(){
+
+    }
+
+    static function activePlugin(){
+      $pluginDb = new databasefunctions();
+      $pluginDb->createTable();
+      file_put_contents(dirname(__file__).'/error_activation.log', ob_get_contents());
+    }
+
+  }
+  new vedoBundles(); ?>
