2388b4038bf088c1c7baaa87fdad9bf85cde191d
Munendra Kumar <kumarmunendra@hotmail.com>
2018-10-15 23:13:36 +0530

    formate code

--- classes/databasefunctions.php
+++ classes/databasefunctions.php
@@ -1,17 +1,18 @@
-<?php class databasefunctions {
-   public function createTable() {
-       global $table_prefix, $wpdb;
+<?php class databasefunctions
+{
+    public function createTable()
+    {
+        global $table_prefix, $wpdb;
 
-$tblname = 'vedoBundles';
-$wp_track_table = $table_prefix . "$tblname ";
+        $tblname = 'vedoBundles';
+        $wp_track_table = $table_prefix . "$tblname ";
 
 #Check to see if the table exists already, if not, then create it
 
-if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
-{
+        if ($wpdb->get_var("show tables like '$wp_track_table'") != $wp_track_table) {
 
-    $charset_collate = $wpdb->get_charset_collate();
-    $sql = "CREATE TABLE $wp_track_table (
+            $charset_collate = $wpdb->get_charset_collate();
+            $sql = "CREATE TABLE $wp_track_table (
 id mediumint(9) NOT NULL AUTO_INCREMENT,
 CreatedAt datetime  DEFAULT now(),
 Title TEXT DEFAULT '' NOT NULL,
@@ -21,9 +22,9 @@ Category Text DEFAULT '',
 DealerName Text DEFAULT '',
 PRIMARY KEY  (id)
 ) $charset_collate;";
-      file_put_contents(dirname(__file__).'/log.txt', $sql);
-      require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
-    dbDelta($sql);
+            file_put_contents(dirname(__file__) . '/log.txt', $sql);
+            require_once ABSPATH . '/wp-admin/includes/upgrade.php';
+            dbDelta($sql);
+        }
+    }
 }
- }
-} ?>
--- index.php
+++ index.php
@@ -1,41 +1,46 @@
-<?php 
- /*
-   Plugin Name: Vido-bundles
-   Plugin URI: https://github.com/munendra/Vedo-Bundles
-   description: A plugin to store video link by category
-   Version: 1.0
-   Author: Munendra Kumar
-   Author URI: https://github.com/munendra/
-   License: GPL2
-   */
-  if(!defined('vedo_bundles'))
-	define('vedo_bundles', plugin_dir_url( __FILE__ ));
-if(!defined('vedo_bundles'))
-  define('vedo_bundles', plugin_dir_path( __FILE__ ));
-  include_once dirname( __FILE__ ) . '/classes/databasefunctions.php';
-  class vedoBundles 
-  {
-    var $pluginDb;
+<?php
+/*
+Plugin Name: Vido-bundles
+Plugin URI: https://github.com/munendra/Vedo-Bundles
+description: A plugin to store video link by category
+Version: 1.0
+Author: Munendra Kumar
+Author URI: https://github.com/munendra/
+License: GPL2
+ */
+if (!defined('vedo_bundles')) {
+    define('vedo_bundles', plugin_dir_url(__FILE__));
+}
+
+if (!defined('vedo_bundles')) {
+    define('vedo_bundles', plugin_dir_path(__FILE__));
+}
+
+include_once dirname(__FILE__) . '/classes/databasefunctions.php';
+class vedoBundles
+{
+    public $pluginDb;
     public function __construct()
     {
-     
-      register_activation_hook( __FILE__, array('vedoBundles','activePlugin'));
- // Admin page calls:
- add_action( 'admin_menu', array( $this, 'addAdminMenu' ) );
- //add_action( 'wp_ajax_store_admin_data', array( $this, 'storeAdminData' ) );
- //add_action( 'admin_enqueue_scripts', array( $this, 'addAdminScripts' ) );
+
+        register_activation_hook(__FILE__, array('vedoBundles', 'activePlugin'));
+        // Admin page calls:
+        add_action('admin_menu', array($this, 'addAdminMenu'));
+        //add_action( 'wp_ajax_store_admin_data', array( $this, 'storeAdminData' ) );
+        //add_action( 'admin_enqueue_scripts', array( $this, 'addAdminScripts' ) );
     }
 
-
-    function addAdminMenu(){
+    public function addAdminMenu()
+    {
 
     }
 
-    static function activePlugin(){
-      $pluginDb = new databasefunctions();
-      $pluginDb->createTable();
-      file_put_contents(dirname(__file__).'/error_activation.log', ob_get_contents());
+    public static function activePlugin()
+    {
+        $pluginDb = new databasefunctions();
+        $pluginDb->createTable();
+        file_put_contents(dirname(__file__) . '/error_activation.log', ob_get_contents());
     }
 
-  }
-  new vedoBundles(); ?>
+}
+new vedoBundles();
